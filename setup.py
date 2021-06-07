5c26a3182ac339cba158d1af5eb38b3804c3067e
윤sungzzuu <sungzzuu@gmail.com>
2021-06-07 18:49:03 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup,Extension
+
+
+setup(name='FindHome',
+version='1.0',
+packages=['MyData/', 'MyImage/','Term', 'Bookmark', 'goglemaps',
+                  'localCode', 'map', 'mysmtplib',
+                  'noti', 'teller']
+)
